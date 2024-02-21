688ff409db7bf26e1ae6e0f8aa15d2f8aed826c1
Raja Kumar <raja.kumar@chrobinson.com>
2024-02-21 19:57:35 +0530

    Signout

---
+++ README.md
@@ -0,0 +1,34 @@
+# Netflix-Gpt
+
+- Create React app
+- Setup Tailwing CSS
+- Added Tailwing Classes to div
+- Header
+- Routing of App
+- Login page
+- Sign Up Form
+- validation for
+- useRef
+- Firebase setup
+- Deploying our app to production
+- Create Signup user Account
+- Implement Signin user Api
+- Created Redux Store with User Slice
+- Implemeted Signout features
+- Update profile
+
+# Features
+
+- Login - Sign Up page
+  - Sign in / Sign Up Page
+  - Redirect to Browse Page
+- Browse Page (After Authentication)
+  - header
+  - Main Movie
+  - Trailer inBackground
+  - Movie tiltle and description
+  - Movies Suggestions
+  - Movie Suggestion \* N
+- Netflix GPT
+  - Search Bar
+  - Movie Suggestions
---
+++ src/components/Header.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const user = useSelector((store) => store.user);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
+
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-full flex justify-between">
+      <img
+        className="w-44"
+        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        alt="logo"
+      />
+      {user && (
+        <div className="p-2 flex">
+          <img className="w-12 h-12" src={user?.photoURL}></img>
+          <button className="text-white m-2" onClick={handleSignOut}>
+            Sign Out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
